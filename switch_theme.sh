017b72a30c1a0f9ff1aa9283724492614e74718f
Darrkhan <pmdrk-github@protonmail.com>
2024-12-18 23:16:41 +0000

    feat(theme): adding alacritty theme following sway theme

--- switch_theme.sh
+++ switch_theme.sh
@@ -2,12 +2,18 @@
 
 LIGHT_THEME="$HOME/.config/sway/theme.d/theme_light"
 DARK_THEME="$HOME/.config/sway/theme.d/theme_dark"
+
+ALACRITTY_LIGHT="$HOME/.config/alacritty/alacritty_light.toml"
+ALACRITTY_DARK="$HOME/.config/alacritty/alacritty_dark.toml"
+
 CURRENT_HOUR=$(date +%H)
 
 if [ "$CURRENT_HOUR" -ge 7 ] && [ "$CURRENT_HOUR" -lt 20 ]; then
     ln -sf "$LIGHT_THEME" "$HOME/.config/sway/theme"
+    ln -sf "$ALACRITTY_LIGHT" "$HOME/.config/alacritty/alacritty.toml"
 else
     ln -sf "$DARK_THEME" "$HOME/.config/sway/theme"
+    ln -sf "$ALACRITTY_DARK" "$HOME/.config/alacritty/alacritty.toml"
 fi
 
 sway reload
